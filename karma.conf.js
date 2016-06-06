a39ceced3c6c3aba6449ffa632ff875004de23fd
komali2 <rogersjcaleb@gmail.com>
2016-06-06 15:32:09 -0700

    core: added karma config

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function(config){
+  config.set({
+
+    basePath: './app',
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'components/**/*.js',
+      'view*/**/*.js'
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine',
+    ]
+  });
+};
