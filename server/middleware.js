4b09401f2b1aa0f0647285ce580df1acdfc34002
komali2 <rogersjcaleb@gmail.com>
2016-06-10 11:17:20 -0700

    core: added socketio client dep

--- server/middleware.js
+++ server/middleware.js
@@ -2,7 +2,12 @@ var path = require('path');
 
 module.exports = function(app, express){
   var http = require('http').Server(app);
+  var io = require('socket.io')(http);
 
   app.use(express.static(path.join(__dirname, '../app')));
+
+  io.on('connection', function(socket){
+    console.log('a user connected');
+  });
 
 };
