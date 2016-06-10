35a49deff8877e19f4dc4ba9491d109ec6b62466
komali2 <rogersjcaleb@gmail.com>
2016-06-10 11:14:22 -0700

    core: added socketio dependency

--- server/middleware.js
+++ server/middleware.js
@@ -1,5 +1,8 @@
 var path = require('path');
 
 module.exports = function(app, express){
+  var http = require('http').Server(app);
+
   app.use(express.static(path.join(__dirname, '../app')));
+
 };
