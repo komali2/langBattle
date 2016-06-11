f33221fc16d24d46b6937dff3e718fb5e59ebddc
komali2 <rogersjcaleb@gmail.com>
2016-06-11 13:15:11 -0700

    core: middleware set up

--- server/middleware.js
+++ server/middleware.js
@@ -1,7 +1,10 @@
 var path = require('path');
 
 module.exports = function(app, express){
+  var arenaRouter = express.Router();
 
   app.use(express.static(path.join(__dirname, '../app')));
+  app.use('/arena', arenaRouter);
 
+  require('./routes/arena/arenaRouter.js');
 };
