6ab99b6acf8a83f3a12122aa78203a75b5572cba
komali2 <rogersjcaleb@gmail.com>
2016-06-13 11:45:01 -0700

    core: created cardlist basic

--- server/routes/arena/arenaController.js
+++ server/routes/arena/arenaController.js
@@ -1,3 +1,10 @@
+var cardList = {
+  1: ['hello', 'nihao'],
+  2: ['dog', 'gou'],
+  3: ['goodbye', 'zaijian'],
+  4: ['please', 'qing']
+}
+
 module.exports = {
   getCards: function(req, res){
     res.status(200).send("");
