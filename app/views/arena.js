f3efed212f132dbaeb1b77b8351b08ac6fc6f0ba
komali2 <rogersjcaleb@gmail.com>
2016-06-13 12:00:11 -0700

    core: set up clientside function for getting cards

--- app/views/arena.js
+++ app/views/arena.js
@@ -1,6 +1,17 @@
 angular.module('langBattle')
-.controller('arenaController', ['$scope', 'socket', function($scope, socket){
-  $scope.submitChat = function(){
-    socket.emit('chat', 'test');
-  }
-}]);
+  .controller('arenaController',
+    ['$scope', 'socket', '$http',
+    function($scope, socket, $http){
+
+      $scope.submitChat = function(){
+        socket.emit('chat', 'test');
+      }
+
+      $scope.getCards = function(num){
+        return $http.get('/arena/cards', {
+          params: {
+            num: num
+          }
+        });
+      }
+    }]);
