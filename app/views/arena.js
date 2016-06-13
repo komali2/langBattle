112c3152b5c73b4ef90b2de8e939edc64863b78d
komali2 <rogersjcaleb@gmail.com>
2016-06-13 12:06:15 -0700

    wip: set up basic structure for getting new cards on client side

--- app/views/arena.js
+++ app/views/arena.js
@@ -2,7 +2,7 @@ angular.module('langBattle')
   .controller('arenaController',
     ['$scope', 'socket', '$http',
     function($scope, socket, $http){
-
+      $scope.cardNum = 0;
       $scope.submitChat = function(){
         socket.emit('chat', 'test');
       }
@@ -13,5 +13,15 @@ angular.module('langBattle')
             num: num
           }
         });
+      }
+
+      $scope.showCards = function(){
+        $scope.getCards($scope.cardNum)
+          .then((res)=>{
+            $scope.currCard = res;
+            $scope.cardNum += 1;
+          }, (res)=>{
+            console.log('error in showCard', res);
+          });
       }
     }]);
