c8267c4e3f8bb017d37c627f6ac0bf1787755ab2
komali2 <rogersjcaleb@gmail.com>
2016-06-14 14:56:17 -0700

    bug: included battle in index

--- app/views/battle.js
+++ app/views/battle.js
@@ -2,9 +2,11 @@ angular.module('langBattle')
   .controller('battleController',
   ['$scope', 'socket', '$http',
   function($scope, socket, $http){
+    $scope.closeModal = function(){
+       $('#modal1').closeModal();
+    }
 
 
 
 
-    
   }]);
