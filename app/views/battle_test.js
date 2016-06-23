52783dcedd8a4ff63f0390f551760444fd4d56c2
komali2 <rogersjcaleb@gmail.com>
2016-06-23 16:28:27 -0700

    wip: jquery functioning in tests

--- app/views/battle_test.js
+++ app/views/battle_test.js
@@ -1,4 +1,4 @@
-describe('battle view', function(){
+describe('battle', function(){
   beforeEach(module('langBattle'));
 
   describe('battleController', function(){
@@ -8,5 +8,31 @@ describe('battle view', function(){
       expect(battleController).toBeDefined();
     }));
 
+  });
+
+  beforeEach(inject(function($templateCache) {
+    templateHtml = $templateCache.get('views/battle.html');
+    if(!templateHtml) {
+        templateHtml = $.ajax('views/battle.html', {async: false}).responseText;
+        $templateCache.put('views/battle.html', templateHtml)
+    }
+  }));
+
+  describe('battleview', function(){
+    var $compile, $rootScope, formElement;
+
+        beforeEach(inject(function(_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+            $rootScope.isOn = false;
+            formElement = angular.element(templateHtml);
+            var element = $compile(formElement)($rootScope);
+            $rootScope.$digest();
+        }));
+
+        it('should find divs', function(){
+          console.log(formElement);
+          expect(formElement.find('#battleView').length).toBe(1);
+        });
   });
 });
