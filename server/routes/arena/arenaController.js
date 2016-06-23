f6c6dc190a773e55cb02c795294de4fa43d2d6d3
komali2 <rogersjcaleb@gmail.com>
2016-06-23 15:17:14 -0700

    bp: all functionsextrapolated outside controller

--- server/routes/arena/arenaController.js
+++ server/routes/arena/arenaController.js
@@ -26,36 +26,12 @@ module.exports = {
     });
 
     socket.on('getFirstCard', ()=>{
-      var user = storage.userStorage[socket.id];
 
-      if(user.inBattle){
-        socket.emit('newCard', user.cardArray[0]);
-        user.cardIndex++;
-      }
-
+      ioController.getFirstCard(socket);
     });
 
     socket.on('submitCard', (data)=>{
-      var user = storage.userStorage[socket.id];
-      if(user.inBattle){
-        //if correct
-        if(user.cardArray[user.cardIndex - 1].id === data.id){
-          if(user.cardIndex < 10){
-            socket
-              .emit('newCard', user.cardArray[user.cardIndex]);
-              user.cardIndex++;
-          }
-          else if(user.cardIndex >= 10){
-            var battleRoom = 'battleRoom' + user.roomNumber;
-            socket.emit('youWin', {'youWon':'youWon'});
-            socket.broadcast.to(battleRoom).emit('youLose', {'youLost': 'youLost'});
-          }
-        }
-        //if incorrect
-        else{
-          socket.emit('wrongCard', {});
-        }
-      }
+      ioController.submitCard(socket, data);
     });
 
 
@@ -65,27 +41,7 @@ module.exports = {
     });
 
     socket.on('startBattle', ()=>{
-      var user = storage.userStorage[socket.id];
-      var battleRoom = 'battleRoom' + user.roomNumber;
-      var counter = 0;
-      user.isReady = true;
-      for(var i = 0; i < storage.roomStorage[user.roomNumber].length; i++){
-        if(storage.roomStorage[user.roomNumber][i].isReady){
-          counter++;
-        }
-      }
-      if(counter === 1){
-        socket.broadcast.to(battleRoom).emit('partnerStarted', {});
-      }
-      else if(counter === 2){
-        cardController.getCardArray(function(cardArray){
-          user.cardArray = cardArray;
-          user.partner.cardArray = cardArray;
-          socket.broadcast.to(battleRoom).emit('okToStart', {});
-          socket.emit('okToStart', {});
-        });
-
-      }
+      ioController.startBattle(socket);
     })
 
   },
