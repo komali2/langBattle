7ac1754017f2c6d22980d6f48b22408f38ae4a57
komali2 <rogersjcaleb@gmail.com>
2016-06-23 15:19:09 -0700

    syn: cleand up controller code

--- server/routes/arena/arenaController.js
+++ server/routes/arena/arenaController.js
@@ -1,32 +1,15 @@
 var ioController = require('./ioController.js');
-var cardController = require('./cardController.js');
-var storage = require('./storage.js');
-
-
-function User(id){
-  this.id = id;
-  this.cardIndex = 0;
-  this.inBattle = false;
-  this.roomNumber = null;
-  this.hasPartner = false;
-  this.isReady = false;
-  this.cardArray = [];
-  this.partner = {};
-}
 
 module.exports = {
-
 
   ioConnect: function (socket){
     //socket.on('joinBattle', ioController.handleNewPlayer);
     console.log('enter player: ', socket.id);
     socket.on('joinBattle', ()=>{
       ioController.handleNewPlayer(socket);
-
     });
 
     socket.on('getFirstCard', ()=>{
-
       ioController.getFirstCard(socket);
     });
 
@@ -34,10 +17,7 @@ module.exports = {
       ioController.submitCard(socket, data);
     });
 
-
-
     socket.on('youLost', ()=>{
-
     });
 
     socket.on('startBattle', ()=>{
