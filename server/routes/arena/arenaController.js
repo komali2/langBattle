2de8fa55cb6fa88abe3a2828ccf041b6d33fccf0
komali2 <rogersjcaleb@gmail.com>
2016-06-23 14:55:22 -0700

    bp: invoking external controller function over socket

--- server/routes/arena/arenaController.js
+++ server/routes/arena/arenaController.js
@@ -1,4 +1,4 @@
-ioController = require('./ioController.js');
+var ioController = require('./ioController.js');
 var cardController = require('./cardController.js');
 var storage = require('./storage.js');
 
@@ -15,46 +15,14 @@ function User(id){
 }
 
 module.exports = {
-  
+
 
   ioConnect: function (socket){
     //socket.on('joinBattle', ioController.handleNewPlayer);
     console.log('enter player: ', socket.id);
     socket.on('joinBattle', ()=>{
+      ioController.handleNewPlayer(socket);
 
-      //create new user
-      storage.userStorage[socket.id] = new User(socket.id);
-      var user = storage.userStorage[socket.id];
-      //find a user without a partner
-      for(var ele in storage.userStorage){
-        var stored = storage.userStorage[ele];
-        if(!stored.inBattle && stored.id !== user.id){
-          stored.inBattle = true;
-          user.inBattle = true;
-          user.roomNumber = stored.roomNumber;
-          storage.roomStorage[user.roomNumber].push(user);
-          storage.openRoom++;
-          socket.broadcast.to('battleRoom' + stored.roomNumber).emit('hasPartner', {});
-          socket.emit('hasPartner', {});
-          user.partner = stored;
-          stored.partner = user;
-          break;
-        }
-      }
-      //if no open users found, you are first. create new room
-      if(!user.inBattle){
-        user.roomNumber = storage.openRoom;
-        storage.roomStorage[storage.openRoom] = [user];
-
-      }
-
-      var battleRoom = 'battleRoom' + user.roomNumber;
-      //put user in the socket room
-      socket.join(battleRoom, (err)=>{
-        if(err){
-          console.log(err);
-        }
-      });
     });
 
     socket.on('getFirstCard', ()=>{
--- server/routes/arena/ioController.js
+++ server/routes/arena/ioController.js
@@ -1,6 +1,51 @@
 var storage = require('./storage.js');
 
+function User(id){
+  this.id = id;
+  this.cardIndex = 0;
+  this.inBattle = false;
+  this.roomNumber = null;
+  this.hasPartner = false;
+  this.isReady = false;
+  this.cardArray = [];
+  this.partner = {};
+}
+
+
 module.exports = {
-  handleNewPlayer: function(){
+  handleNewPlayer: function(socket){
+    //create new user
+    storage.userStorage[socket.id] = new User(socket.id);
+    var user = storage.userStorage[socket.id];
+    //find a user without a partner
+    for(var ele in storage.userStorage){
+      var stored = storage.userStorage[ele];
+      if(!stored.inBattle && stored.id !== user.id){
+        stored.inBattle = true;
+        user.inBattle = true;
+        user.roomNumber = stored.roomNumber;
+        storage.roomStorage[user.roomNumber].push(user);
+        storage.openRoom++;
+        socket.broadcast.to('battleRoom' + stored.roomNumber).emit('hasPartner', {});
+        socket.emit('hasPartner', {});
+        user.partner = stored;
+        stored.partner = user;
+        break;
+      }
+    }
+    //if no open users found, you are first. create new room
+    if(!user.inBattle){
+      user.roomNumber = storage.openRoom;
+      storage.roomStorage[storage.openRoom] = [user];
+
+    }
+
+    var battleRoom = 'battleRoom' + user.roomNumber;
+    //put user in the socket room
+    socket.join(battleRoom, (err)=>{
+      if(err){
+        console.log(err);
+      }
+    });
   }
 }
