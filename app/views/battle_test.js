993772c179c40f6d5448014a40c22f9e43f405b5
komali2 <rogersjcaleb@gmail.com>
2016-06-24 10:58:59 -0700

    Revert "wip: jquery functioning in tests"
    This reverts commit 52783dcedd8a4ff63f0390f551760444fd4d56c2.

--- app/views/battle_test.js
+++ app/views/battle_test.js
@@ -1,4 +1,4 @@
-describe('battle', function(){
+describe('battle view', function(){
   beforeEach(module('langBattle'));
 
   describe('battleController', function(){
@@ -8,31 +8,5 @@ describe('battle', function(){
       expect(battleController).toBeDefined();
     }));
 
-  });
-
-  beforeEach(inject(function($templateCache) {
-    templateHtml = $templateCache.get('views/battle.html');
-    if(!templateHtml) {
-        templateHtml = $.ajax('views/battle.html', {async: false}).responseText;
-        $templateCache.put('views/battle.html', templateHtml)
-    }
-  }));
-
-  describe('battleview', function(){
-    var $compile, $rootScope, formElement;
-
-        beforeEach(inject(function(_$compile_, _$rootScope_) {
-            $compile = _$compile_;
-            $rootScope = _$rootScope_;
-            $rootScope.isOn = false;
-            formElement = angular.element(templateHtml);
-            var element = $compile(formElement)($rootScope);
-            $rootScope.$digest();
-        }));
-
-        it('should find divs', function(){
-          console.log(formElement);
-          expect(formElement.find('#battleView').length).toBe(1);
-        });
   });
 });
