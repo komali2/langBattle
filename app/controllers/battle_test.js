1530d229003fc750706c2b9fd7e198ca7fd0644a
komali2 <rogersjcaleb@gmail.com>
2016-06-25 15:43:09 -0700

    core: extrapolated more beforeeach logic

--- app/controllers/battle_test.js
+++ app/controllers/battle_test.js
@@ -4,18 +4,26 @@ describe('battle view', function(){
     $controller = _$controller_;
     $rootScope = _$rootScope_;
   }));
+  
+  var scope;
+  var battleController;
 
   describe('battleController', function(){
+    beforeEach(function(){
+      scope = $rootScope.$new();
+      battleController = $controller('battleController', {$scope:scope});
+    })
+
     it('should exist', function(){
-      var scope = $rootScope.$new();
-      var battleController = $controller('battleController', {$scope:scope});
+      // var scope = $rootScope.$new();
+      // var battleController = $controller('battleController', {$scope:scope});
       expect(battleController).toBeDefined();
 
 
     });
     it('should start the battle', function(){
-      var scope = $rootScope.$new();
-      var battleController = $controller('battleController', {$scope:scope});
+      // var scope = $rootScope.$new();
+      // var battleController = $controller('battleController', {$scope:scope});
       scope.getFirstCard();
       expect(scope.battleStarted).toBe(true);
     })
