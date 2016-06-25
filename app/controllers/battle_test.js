a043d6ddbd54e83211927ca3e0ca05c3686180ab
komali2 <rogersjcaleb@gmail.com>
2016-06-25 15:40:55 -0700

    bp: another test functioning with room to expand beforeEach

--- app/controllers/battle_test.js
+++ app/controllers/battle_test.js
@@ -1,12 +1,24 @@
 describe('battle view', function(){
   beforeEach(module('langBattle'));
+  beforeEach(inject(function(_$rootScope_, _$controller_){
+    $controller = _$controller_;
+    $rootScope = _$rootScope_;
+  }));
 
   describe('battleController', function(){
-    it('should exist', inject(function($controller, $rootScope){
+    it('should exist', function(){
       var scope = $rootScope.$new();
       var battleController = $controller('battleController', {$scope:scope});
       expect(battleController).toBeDefined();
-    }));
+
+
+    });
+    it('should start the battle', function(){
+      var scope = $rootScope.$new();
+      var battleController = $controller('battleController', {$scope:scope});
+      scope.getFirstCard();
+      expect(scope.battleStarted).toBe(true);
+    })
 
   });
 });
